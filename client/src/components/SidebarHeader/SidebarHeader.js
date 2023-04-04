aa5742e96178d1d23b7ed7410ee79f527cf097f1
linro911 <linro911@student.liu.se>
2023-04-04 12:51:00 +0200

    Sidebar header done

--- client/src/components/SidebarHeader/SidebarHeader.js
+++ client/src/components/SidebarHeader/SidebarHeader.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./SidebarHeader.css";
 import logo from '../../image/logo.png'
 
 export default function SidebarHeader() {
   return (
-    <div>
-      <p className='CompanyName'>Neue</p>
-      <img src={logo} id="logo" className='CompanyLogo'></img>
+    <div className="SidebarHeaderContainer">
+      <p className="CompanyName">Neue</p>
+      <img src={logo} id="logo" className="CompanyLogo"></img>
     </div>
   )
 }
