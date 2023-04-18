70ac47e9e064213b03251f2a0e43f58372cfe1c9
sagarbatra01 <87910501+sagarbatra01@users.noreply.github.com>
2023-04-18 13:05:14 +0200

    Kommenterat bort topcards och lagt till graf med riktig data.

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -3,39 +3,25 @@ import Sidebar from '../../components/Sidebar/Sidebar';
 import Map from '../../components/Map/Map'
 import './HomePage.css';
 import BarChart from "../../components/BarChart/BarChartComp";
+import LineChart from "../../components/LineChart/LineChartComp";
 import TopCard from "../../components/topCard/topCard";
 import HomePageHeader from "../../components/HomePageHeader/HomePageHeader";
 import SidebarHeader from "../../components/SidebarHeader/SidebarHeader";
 
 function HomePage(props) {
     // Data fetch for graph
-	const [data, setData] = useState([{
-		unit: 0,
-		flow: 0,
-		airpressure: 0,
-		temp: 0,
-		timestamp: 0
-	}]);
+	const [data, setData] = useState([]);
 
 	// Using useEffect for single rendering
 	useEffect(() => API_call("/data"), [])
 
 	function API_call(request) {
+		console.log("api call")
 		fetch(request)
 			.then(res => res.json())
-			.then(res => {
-				// Setting a data from api
-				// console.log(data);
-				setData([
-					{name: "unit", num: res.unit},
-					{name: "flow", num: res.flow},
-					{name: "airpressure", num: res.airpressure},
-					{name: "temp", num: res.temp},
-					{name: "timestamp", num: res.timestamp}
-				])
-				}
-	)
+			.then(data => setData(data))
 	}
+	
     return (
         <div className="HomePageContent">
 			<Map/>
@@ -44,17 +30,18 @@ function HomePage(props) {
 				<HomePageHeader />
 
 				<div className="GridContainer">
-
+					{/*
 					<div className="topCards">
 						<TopCard />
 						<TopCard />
 						<TopCard />
 						<TopCard />
 					</div>
-					<BarChart data={data}/>
-					<BarChart data={data}/>
-					<BarChart data={data}/>
-					<BarChart data={data}/>
+					*/}
+					<LineChart data={data} xkey={"timestamp"} ykey={"airpressure"} title={"Air Pressure"} width={700} height={300}/>
+					{/*<LineChart data={data} xkey={"timestamp"} ykey={"temp"} title={"Temperature"}/>
+					<LineChart data={data} xkey={"timestamp"} ykey={"flow"} title={"Flow"}/>
+					<BarChart data={data} xkey={"timestamp"} ykey={"unit"} title={"Unit"}/>*/}
 				</div>
 			</div>
         </div>
