15c8c377af243a19d24ed7ec5f5d45514037c970
jakos322 <jakos322@student.liu.se>
2023-04-19 16:41:20 +0200

    fixed structruce and styling

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -5,77 +5,11 @@ function Table(props){
 
     const data = useMemo( () => (
         props.data
-        /*
-        [
-            {
-                "unit": 4,
-                "flow": 5,
-                "airpressure": 6,
-                "temp": 7,
-                "Timestamp": 1
-            },
-            {
-                "unit": 1,
-                "flow": 2,
-                "airpressure": 3,
-                "temp": 4,
-                "Timestamp": 2
-            },
-            {
-                "unit": 8,
-                "flow": 8,
-                "airpressure": 8,
-                "temp": 8,
-                "Timestamp": 3
-            },
-            {
-                "unit": 2,
-                "flow": 2,
-                "airpressure": 2,
-                "temp": 2,
-                "Timestamp": 4
-            },
-            {
-                "unit": 5,
-                "flow": 5,
-                "airpressure": 5,
-                "temp": 5,
-                "Timestamp": 5
-            }
-    
-        ]    
-        */
-
     ));
 
     const column = useMemo(()=>(
         props.columns
-        /*
-        [
-            {
-                Header: "Country",
-                accessor: "data.unit"
-            },
-            {
-                Header: "Avg Time",
-                accessor: "data.flow"
-            },
-            {
-                Header: "Tapped (l)",
-                accessor: "data.airpressure"
-            }
-            ]
-            */
     ));
-
-   // const tableInstance = useTable({column, data});
-
-
-
-
-    //const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
-
-
     return (
         <div className = "Tbl">
             <table>
@@ -93,21 +27,7 @@ function Table(props){
                         })}
                     </tr>)
                 })
-                }
-                
-               
-                {/*}
-                {data.map((valData) => {
-                        <tr>
-                        for (let i = 0; i < column.length; i ++){
-                            return (<td>{valData.column[i].accessor}</td>)
-                        }
-                        </tr>
-                    })}*/}
-
-                {
-                    
-            }
+                }                   
             </table>
         </div>
     );
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,43 +2,15 @@ import React, { useState, useEffect, PureComponent } from "react";
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Map from '../../components/Map/Map'
 import './HomePage.css';
-import BarChart from "../../components/BarChart/BarChartComp";
-import LineChart from "../../components/LineChart/LineChartComp";
-import TopCard from "../../components/topCard/topCard";
 import HomePageHeader from "../../components/HomePageHeader/HomePageHeader";
-import SidebarHeader from "../../components/SidebarHeader/SidebarHeader";
-import Table from "../../components/Table/Table";
-import SidebarMap from "../../components/SidebarMap/SidebarMap";
+
 
 function HomePage(props) {
-	let location = "World"
-	const tableColumns = [
-        {
-            Header: "Amount",
-            accessor : "unit"
-        },
-        {
-            Header: "Average Flow",
-            accessor : "flow"
-        },
-        {
-            Header: "Air Pressure",
-            accessor : "airpressure"
-        },
-		{
-            Header: "Temperature",
-            accessor : "temp"
-        }
-        ];
-		/*col 1: "amount": "unit"
-*/
-
-    // Data fetch for graph
+	// Data fetch for graph
 	const [data, setData] = useState([]);
 
 	// Using useEffect for single rendering
 	useEffect(() => API_call("/data"), [])
-	//useEffect(() => API_call("/alldata"), [])
 	
 	function API_call(request) {
 		console.log("api call")
@@ -48,20 +20,12 @@ function HomePage(props) {
 	}
     return (
         <div className="HomePageContent">
-			<div className="SidebarHeader"><SidebarHeader/></div>
-			<div className="Map"><Map/></div>
-			
-			<div className="SensorData">
-				<HomePageHeader location={location}/>
-				<div className="Sidebar">
-					<div className="TableContainer">
-						<Table data = {data} columns = {tableColumns}/>
-					</div>
-					<div className="GridContainer">
-						<LineChart data={data} xkey={"timestamp"} ykey={"airpressure"} title={"Air Pressure"} width={700} height={300}/>
-					</div>
-				</div>
+			<div>
+				<div className="HomePageHeader"><HomePageHeader/></div>
+				<div className="Map"><Map/></div>
 			</div>
+			
+			<div className="Sidebar"><Sidebar data={data}/></div>			
         </div>
 
     );
