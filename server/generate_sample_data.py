4086acd89800f831abb11e602aef1a7fc5df5e80
linro911 <linro911@student.liu.se>
2023-04-20 11:50:39 +0200

    Fix flake8

--- server/generate_sample_data.py
+++ server/generate_sample_data.py
@@ -15,7 +15,8 @@ csv_writer = csv.writer(csv_file)
 header = ["name", "1d", "1w", "1m", "1y", "all"]
 csv_writer.writerow(header)
 
-# If you cannot generate, change adm2 for adm1 and NAME_2 for NAME_1. Remember to change back!
+# If you cannot generate, change adm2 for adm1 
+# and NAME_2 for NAME_1. Remember to change back!
 for i in json_data["objects"][f"{file_name}_adm2"]["geometries"]:
     name = i["properties"]["NAME_2"]
     csv_writer.writerow([name,
