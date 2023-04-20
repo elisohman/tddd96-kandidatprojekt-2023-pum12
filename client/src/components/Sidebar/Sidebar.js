4283e1bd9cfdd6b49bfceb80de3026959ae7f9a4
fredr <freho169@student.liu.se>
2023-04-20 14:21:18 +0200

    SidebarButtons is working, and has some documentation

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -4,6 +4,14 @@ import "./Sidebar.css";
 import LineChart from "../../components/LineChart/LineChartComp";
 import SidebarHeader from "../../components/SidebarHeader/SidebarHeader";
 import Table from "../../components/Table/Table";
+import TimespanButtons from "../TimespanButtons/TimespanButtons";
+
+
+//  i denna funktion updaterar man sen data för tabell & graf beroende på knapptryckning
+//  Matha med title arrayens ordning, index = 0 är title[0] o.s.v.
+function ButtonFunction(index){
+    console.log(`${index == 1 ? "Knapp1" : "annan knapp"}`);
+}
 
 function Sidebar(props) {
     let location = "World"
@@ -30,6 +38,7 @@ function Sidebar(props) {
             <SidebarHeader location={location}/>
                 <div className="TableContainer">
                     <Table data = {props.data} columns = {tableColumns}/>
+                    <TimespanButtons parentFunction = {ButtonFunction} title = {["1 d", "1 w", "1 m", "1 y", "All"]}/>
                 </div>
                 <div className="LineChartContainer">
                     <LineChart data={props.data} xkey={"timestamp"} ykey={"airpressure"} title={"Air Pressure"} width={700} height={300}/>
