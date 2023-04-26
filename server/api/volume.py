2763fa6faa65a41bc922bc5312e21ba8fd917a74
August Gustafsson <auggu628@student.liu.se>
2023-04-26 13:12:54 +0200

    fixed /volume/series and /volume/total route to work with updated cloud procedures

--- server/api/volume.py
+++ server/api/volume.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request
-from services.volume_service import (get_series_for_world_view_service,
-                                     get_series_for_country_view_service,
-                                     get_total_for_world_view_service)
+from services.volume_service import (get_series_service,
+                                     get_total_service)
 
 volume_route = Blueprint('volume_route', __name__)
 
@@ -9,23 +8,22 @@ volume_route = Blueprint('volume_route', __name__)
 @volume_route.route("/volume/series", methods=['GET'])
 def get_series_for_world_view():
     time_range = request.args.get('time_range')
-    return get_series_for_world_view_service(time_range)
+    return get_series_service(time_range)
 
 
 @volume_route.route("/volume/series/<country>", methods=['GET'])
 def get_series_for_country_view(country):
     time_range = request.args.get('time_range')
-    return get_series_for_country_view_service(country, time_range)
+    return get_series_service(time_range, f'"{country}"')
 
 
 @volume_route.route("/volume/total", methods=['GET'])
 def get_total_for_world_view():
     time_range = request.args.get('time_range')
-    return get_total_for_world_view_service(time_range)
+    return get_total_service(time_range)
 
 
 @volume_route.route("/volume/total/<country>", methods=['GET'])
 def get_total_for_country_view(country):
-    pass
-    # time_range = request.args.get('time_range')
-    # return get_total_for_country_view_service(country, time_range)
+    time_range = request.args.get('time_range')
+    return get_total_service(time_range, f'"{country}"')
--- server/services/volume_service.py
+++ server/services/volume_service.py
@@ -5,60 +5,39 @@ import os
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../service-account-key.json'
 
 
-def get_series_for_world_view_service(time_range):
-    client = bigquery.Client()
-    query_job = client.query(
-        f"""
+def get_series_service(time_range, country="NULL"):
+    query = f"""
         CALL `internet-of-kegs.Testing123.graphGetCountries{
-            get_procedure_time_range(time_range)
-            }Volume`();
+        get_procedure_time_range(time_range)
+        }Volume`({country});
         """
-    )
-    results = query_job.result()  # Waits for job to complete.
 
-    return [dict(row) for row in results]
+    results = get_bigquery_data(query)
+    for row in results:
+        row['total_volume'] /= 1000
 
-
-def get_series_for_country_view_service(country, time_range):
-    client = bigquery.Client()
-    query_job = client.query(
-        f"""
-        CALL `internet-of-kegs.Testing123.GetCountry{
-            get_procedure_time_range(time_range)
-            }Volume`(\'{country}\');
-        """
-    )
-    results = query_job.result()  # Waits for job to complete.
-
-    return [dict(row) for row in results]
+    return results
 
 
-def get_total_for_world_view_service(time_range):
-    procedure_time_range = "All"
-    if time_range == "1d":
-        procedure_time_range = "24hours"
-    elif time_range == "1w":
-        procedure_time_range = "1Week"
-    elif time_range == "1m":
-        procedure_time_range = "1Month"
-    elif time_range == "1y":
-        procedure_time_range = "1Year"
-
-    client = bigquery.Client()
-    query_job = client.query(
-        f"""
+def get_total_service(time_range, country="NULL"):
+    query = f"""
         CALL `internet-of-kegs.Testing123.tableGetCountries{
-            procedure_time_range
-            }Volume`();
+        get_procedure_time_range(time_range)
+        }Volume`({country});
         """
-    )
+
+    results = get_bigquery_data(query)
+
+    return results
+
+
+def get_bigquery_data(query):
+    client = bigquery.Client()
+    query_job = client.query(query)
+
     results = query_job.result()  # Waits for job to complete.
 
     return [dict(row) for row in results]
-
-
-def get_total_for_country_view_service(time_range, country):
-    pass
 
 
 def get_procedure_time_range(time_range):
