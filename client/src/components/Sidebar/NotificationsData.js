a52a06a865b4a048400d010806ad1b9163e3060f
linro911 <linro911@student.liu.se>
2023-05-11 16:03:44 +0200

    Made notifications more interesting with better dummy data

--- client/src/components/Sidebar/NotificationsData.js
+++ client/src/components/Sidebar/NotificationsData.js
@@ -2,89 +2,89 @@ import React from 'react';
 
 export const NotificationsData = [
     {
-        location: "Sweden",
-        unit: 42,
+        location: "Germany",
+        unit: 14,
         alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
+        timestamp: "10/05/2023 19:53",
         read: false
     },
     {
         location: "Sweden",
         unit: 42,
+        alarm: "In need of cleaning!",
+        timestamp: "10/05/2023 19:24",
+        read: false
+    },    {
+        location: "Sweden",
+        unit: 41,
         alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
+        timestamp: "10/05/2023 19:12",
+        read: false
+    },    {
+        location: "Spain",
+        unit: 71,
+        alarm: "Temperature too low!",
+        timestamp: "10/05/2023 12:58",
+        read: false
+    },    {
+        location: "Germany",
+        unit: 9,
+        alarm: "Temperature too high!",
+        timestamp: "09/05/2023 18:42",
         read: false
     },    {
         location: "Sweden",
         unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
+        alarm: "Temperature too low!",
+        timestamp: "27/04/2023 12:17",
+        read: true
+    },    {
+        location: "Spain",
+        unit: 78,
+        alarm: "In need of cleaning!",
+        timestamp: "25/04/2023 17:32",
+        read: true
     },    {
         location: "Sweden",
-        unit: 42,
+        unit: 39,
+        alarm: "Temperature too low!",
+        timestamp: "20/04/2023 14:23",
+        read: true
+    },    {
+        location: "Germany",
+        unit: 11,
         alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
+        timestamp: "08/04/2023 19:49",
+        read: true
+    },    {
+        location: "Spain",
+        unit: 75,
+        alarm: "In need of cleaning!",
+        timestamp: "28/03/2023 10:20",
+        read: true
     },    {
         location: "Sweden",
-        unit: 42,
+        unit: 36,
         alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
+        timestamp: "24/03/2023 16:48",
+        read: true
+    },    {
+        location: "Germany",
+        unit: 13,
+        alarm: "Temperature too high!",
+        timestamp: "22/03/2023 21:43",
+        read: true
+    },    {
+        location: "Germany",
+        unit: 4,
+        alarm: "Temperature too low!",
+        timestamp: "19/03/2023 20:03",
+        read: true
     },    {
         location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
-    },    {
-        location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
-    },    {
-        location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
-    },    {
-        location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
-    },    {
-        location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
-    },    {
-        location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
-    },    {
-        location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
-    },    {
-        location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
-    },    {
-        location: "Sweden",
-        unit: 42,
-        alarm: "Temperature too high!",
-        timestamp: "28/03/2023 19:53",
-        read: false
+        unit: 40,
+        alarm: "In need of cleaning!",
+        timestamp: "19/03/2023 08:32",
+        read: true
     },
 ]
